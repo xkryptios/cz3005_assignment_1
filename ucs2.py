7858288f3cef8416e98e80113d439c9ecde74dce
xiang <linruixiang123@gmail.com>
2022-02-16 00:58:07 +0800

    Edit on task2 by considering the budget constraint

--- ucs2.py
+++ ucs2.py
@@ -7,11 +7,14 @@ import json
 
 class Graph:
 
-    def __init__(self, graph, dist):
+    BUDGET_CONSTRAINT = 287932
+
+    def __init__(self, graph, dist, cost):
         # graph -> disctionary => graph["node"] = ["node1","node2"]
         self.graph = graph
         # dist -> dictionary => dist["node,node2"]
         self.dist = dist
+        self.cost = cost
         self.V = len(graph)
 
     def uniform_cost_search(self, start, goal):
@@ -26,8 +29,8 @@ class Graph:
         visited = []
 
         pq = []
-        # [cost to this node, node_index, parent_node]
-        pq_node = [0, start, "-1"]
+        # [cost to this node,total budget to this node, node_index, parent_node]
+        pq_node = [0, 0, start, "-1"]
 
         pq.append(pq_node)
 
@@ -36,8 +39,9 @@ class Graph:
             current_pq_node = pq.pop(0)
 
             cost_to_this_node = current_pq_node[0]
-            node_index = current_pq_node[1]
-            parent_node_index = current_pq_node[2]
+            total_budget_to_this_node = current_pq_node[1]
+            node_index = current_pq_node[2]
+            parent_node_index = current_pq_node[3]
 
             if (node_index == goal):
                 final_cost = cost_to_this_node
@@ -51,9 +55,17 @@ class Graph:
 
                 for child in self.graph[node_index]:
                     string_param = node_index+','+child
+
+                    total_budget_to_child = self.cost[string_param] + \
+                        total_budget_to_this_node
+
+                    if total_budget_to_child > self.BUDGET_CONSTRAINT:  # if budget too high, do not add this node to the pq
+                        continue
+
                     cost_to_child = self.dist[string_param] + cost_to_this_node
 
-                    pq_node = [cost_to_child, child, node_index]
+                    pq_node = [cost_to_child,
+                               total_budget_to_child, child, node_index]
                     pq.append(pq_node)
 
     def get_path(self, parent, start, goal):
@@ -71,10 +83,10 @@ if __name__ == "__main__":
     g = json.load(open("G.json"))
     dist = json.load(open("Dist.json"))
     cost = json.load(open("Cost.json"))
-    combined_cost = {}
+    # combined_cost = {}
     # print(len(dist),len(cost))
-    for key in dist:
-        combined_cost[key] = dist[key] + cost[key]
+    # for key in dist:
+    #     combined_cost[key] = dist[key] + cost[key]
 
-    graph = Graph(g, combined_cost)
+    graph = Graph(g, dist, cost)
     graph.uniform_cost_search(1, 50)
